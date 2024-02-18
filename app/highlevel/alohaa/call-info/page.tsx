4ab24bbee9dc998ba83bed068d5be818c046da64
naman360 <naman.geek@gmail.com>
2024-02-18 23:35:48 +0530

    feat(alohaa-next-mig): manage account func done

---
+++ app/highlevel/alohaa/call-info/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CallInfo: React.FC = () => {
+  return <div>CallInfo</div>;
+};
+
+export default CallInfo;
---
+++ app/highlevel/alohaa/manage-account/page.tsx
@@ -0,0 +1,287 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { apiUrl } from "@/config";
+import axios from "axios";
+import Alert from "@/components/Alert";
+
+interface locationDataIF {
+  didNumbers: string | null;
+  callerNumbers: string | null;
+  users:
+    | [
+        {
+          email: string;
+          didNumber: string | null;
+          callerNumber: string | null;
+        }
+      ]
+    | [];
+}
+
+const ManageAccount: React.FC = () => {
+  const allohaApiKeyRef = useRef("");
+  const emailRef = useRef("");
+  const [locationData, setLocationData] = useState<locationDataIF>({
+    didNumbers: null,
+    callerNumbers: null,
+    users: [],
+  });
+
+  const [showError, setShowError] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [currentLocationId, setCurrentLocationId] = useState<string | null>(
+    null
+  );
+  const alertMsg = useRef("");
+  // const didNumberRefMakeCall = useRef('918069738302'); // Create a ref for Make a Call DID number
+  // const callerNumberRefMakeCall = useRef(null); // Create a ref for Make a Call Caller number
+  // const receiverNumberRefMakeCall = useRef(null) // Receiver Number Ref
+  const didNumberRefAddNumbers = useRef<HTMLInputElement | null>(null); // Create a ref for Add Numbers DID number
+  const callerNumberRefAddNumbers = useRef<HTMLInputElement | null>(null); // Create a ref for Add Numbers Caller number
+  const addUserEmailRef = useRef<HTMLInputElement | null>(null); // add new user email
+  const addUserDidMobileNumberRef = useRef<HTMLInputElement | null>(null); //add new user did mobile number
+  const addUserCallerMobileNumberRef = useRef<HTMLInputElement | null>(null); // add new user caller mobile number
+
+  const handleAddNumbers = async () => {
+    try {
+      // Access the values using ref.current.value
+      const didNumber = didNumberRefAddNumbers?.current?.value;
+      const callerNumber = callerNumberRefAddNumbers?.current?.value;
+      if (!didNumber && !callerNumber) return;
+      await axios.post(`${apiUrl}/api/crmalloha/update`, {
+        didNumber: didNumber,
+        callerNumber: callerNumber,
+        locationId: currentLocationId,
+      });
+      alertMsg.current = "Details Update Success";
+      setShowSuccess(true);
+      console.log("current", currentLocationId);
+      fetchUserInfo(currentLocationId); // to fetch updated data
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 3000);
+    } catch (err: any) {
+      console.log(err);
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+      alertMsg.current = err?.response?.data?.error || "Some Error Occured";
+    }
+  };
+
+  const handleAddUser = async () => {
+    try {
+      const userEmail = addUserEmailRef?.current?.value;
+      const userDidMobile = addUserDidMobileNumberRef?.current?.value;
+      const userCallerMobile = addUserCallerMobileNumberRef?.current?.value;
+      const locationId = currentLocationId;
+
+      if (
+        !currentLocationId ||
+        !userEmail ||
+        !userDidMobile ||
+        !userCallerMobile
+      ) {
+        alertMsg.current = "Missing Payload";
+        setShowError(true);
+        setTimeout(() => {
+          setShowError(false);
+        }, 3000);
+        return;
+      }
+
+      await axios.post(`${apiUrl}/api/crmalloha/add-alloha-user`, {
+        didNumber: userDidMobile,
+        locationId: locationId,
+        callerNumber: userCallerMobile,
+        userEmail: userEmail,
+      });
+      alertMsg.current = "User Added Successfully";
+      setShowSuccess(true);
+      fetchUserInfo(currentLocationId);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 3000);
+    } catch (err: any) {
+      console.log(err);
+      setShowError(true);
+      setTimeout(() => {
+        setShowError(false);
+      }, 3000);
+      alertMsg.current = err?.response?.data?.error || "Some Error Occured";
+    }
+  };
+
+  const fetchUserInfo = async (locationId: string | null) => {
+    console.log(locationId);
+    try {
+      const { data } = await axios.get(
+        `${apiUrl}/api/crmalloha/fetchlocationinfo/${locationId}`
+      );
+      if (data?.data?.success) {
+        if (data?.data?.data?.apiKey) {
+          allohaApiKeyRef.current = data?.data?.data?.apiKey;
+          emailRef.current = data?.data?.data?.email;
+          setLocationData(data?.data?.data);
+        } else {
+          alertMsg.current = "Please Add Your Api Key";
+          setShowError(true);
+          setTimeout(() => {
+            setShowError(false);
+          }, 10000);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Hello");
+    const locationId = new URL(window.location.href).searchParams.get(
+      "locationId"
+    );
+    setCurrentLocationId(locationId);
+    fetchUserInfo(locationId);
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          background: "dodgerblue",
+          width: "100vw",
+          height: "50px",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src="https://msgsndr-private.storage.googleapis.com/companyPhotos/83ddb2ac-fcce-4455-aff1-b635357a4719.png"
+          alt="clientflowx_logo_img"
+          style={{ width: "200px" }}
+        />
+        {/* <h2 style={{color:"white",fontSize:"3rem"}}>X</h2> &nbsp;&nbsp; */}
+        &nbsp;&nbsp;&nbsp;
+        <img
+          src="https://business.alohaa.ai/static/media/alohaaLogoAndWhiteText.92d0e338.svg"
+          style={{ width: "200px" }}
+          alt="alloha_logo_img"
+        />
+      </div>
+      <h2 style={{ textAlign: "center", fontSize: "2.3rem" }}>
+        Manage Your Alohaa Integration With ClientFlowX
+      </h2>
+      {(showError || showSuccess) && (
+        <Alert
+          type={showError ? "error" : "success"}
+          message={alertMsg?.current}
+        />
+      )}
+      <div>
+        <div className="w-[345px] rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2"> Add Numbers</div>
+            <div>
+              <input
+                type="text"
+                id="first_name"
+                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Enter DID Number"
+                ref={didNumberRefAddNumbers}
+                required
+              />
+              <input
+                type="text"
+                id="first_name"
+                className="border border-gray-300 text-gray-900 mt-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Enter Caller Number"
+                ref={callerNumberRefAddNumbers}
+                required
+              />
+              <button
+                type="submit"
+                onClick={handleAddNumbers}
+                className="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Add Now
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-[345px] rounded overflow-hidden shadow-lg">
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2"> Add Users</div>
+            <div>
+              <input
+                type="text"
+                id="first_name"
+                className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Enter Email"
+                ref={addUserEmailRef}
+                required
+              />
+              <input
+                type="text"
+                id="first_name"
+                className="border border-gray-300 text-gray-900 mt-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Enter DID Number"
+                ref={addUserDidMobileNumberRef}
+                required
+              />
+              <input
+                type="text"
+                id="first_name"
+                className="border border-gray-300 text-gray-900 mt-2 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder="Enter Caller Number"
+                ref={addUserCallerMobileNumberRef}
+                required
+              />
+              <button
+                type="submit"
+                onClick={handleAddUser}
+                className="text-white mt-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Add Now
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="relative overflow-x-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+            <thead className="text-xs text-gray-700 uppercase ">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  DID No.
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Caller No.
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {locationData?.users?.map((item, idx) => {
+                return (
+                  <tr className="bg-white border-b" key={`item-${idx}`}>
+                    <td className="px-6 py-4">{item?.email}</td>
+                    <td className="px-6 py-4">{item?.didNumber}</td>
+                    <td className="px-6 py-4">{item?.callerNumber}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ManageAccount;
