fec032285a72c11b64db376333784f57fbf45f93
namancfx <naman.shankhydhar@clientflowx.com>
2024-02-26 16:50:05 +0530

    fix: appended location id in fetchIncomingCallsInfo

--- app/highlevel/alohaa/call-info/page.tsx
+++ app/highlevel/alohaa/call-info/page.tsx
@@ -63,7 +63,7 @@ const CallInfo = () => {
     try {
       setLoader(true);
       const { data } = await axios.get(
-        `${apiUrl}/api/crmalloha/fetchlocationinfo/L5KNiAsUDWR56VLxY299`
+        `${apiUrl}/api/crmalloha/fetchlocationinfo/${locationId}`
       );
 
       const callLogsData = data?.data?.data?.callDetails || [];
