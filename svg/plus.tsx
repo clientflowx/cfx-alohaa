3ee3c30ca2df7d566f6d9a6439b89cd4b8e6a080
namancfx <naman.shankhydhar@clientflowx.com>
2024-03-22 16:21:47 +0530

    feat(invoices): svgs added

--- svg/plus.tsx
+++ svg/plus.tsx
@@ -1,7 +1,7 @@
 export default (
   <>
     <path
-      fill-rule="evenodd"
+      fillRule="evenodd"
       d="M 11 2 L 11 11 L 2 11 L 2 13 L 11 13 L 11 22 L 13 22 L 13 13 L 22 13 L 22 11 L 13 11 L 13 2 Z"
     ></path>
   </>
